e3e2f1c9308f1a86ac58426cb33807945452a252
Nahuel Carrizo <nhcarrizo@gmail.com>
2023-07-06 02:39:28 -0300

    ljno

--- web/src/components/shared/slider/collection1/scrolleableContent.tsx
+++ web/src/components/shared/slider/collection1/scrolleableContent.tsx
@@ -112,7 +112,7 @@ function ScrolleableContent({ items }) {
       const numSections = horizontalSections.length
 
       const tl = gsap.to(horizontalSections, {
-        xPercent: -39 * numSections,
+        xPercent: -41 * numSections,
 
         ease: 'none',
       })
